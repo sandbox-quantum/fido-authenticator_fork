2f0f7b2c4c94df3d9c7c5b1ec15943a416581c1c
gcsbt <150135295+gcsbt@users.noreply.github.com>
2023-12-06 14:44:20 +0100

    pqclean: add support for aarch64-apple-darwin target

--- pqclean/build.rs
+++ pqclean/build.rs
@@ -2,12 +2,25 @@ use std::env;
 // use std::path::PathBuf;
 
 fn main() {
-
-    // Set the path to the directory containing the C library
     let current_dir = env::current_dir().unwrap();
 
-    // Tell the build script to link against the C library
-    println!("cargo:rustc-link-search=native={}", current_dir.display());
-    println!("cargo:rustc-link-lib=static=pqc_lib2");
-    println!("cargo:rustc-link-lib=static=kyber_clean");
+    // Cargo supports overriding the build script specified with a custom library,
+    // however I didn't manage to make it work.
+    // https://doc.rust-lang.org/cargo/reference/build-scripts.html#overriding-build-scripts
+
+    let target = env::var("TARGET").unwrap();
+    match target.as_str() {
+        "thumbv8m.main-none-eabi" => {
+            println!("cargo:rustc-link-search=native={}/arm/", current_dir.display());
+            println!("cargo:rustc-link-lib=static=pqc_lib2");
+            println!("cargo:rustc-link-lib=static=kyber_clean");
+        }
+        "aarch64-apple-darwin" => {
+            println!("cargo:rustc-link-search=native={}/aarch64/", current_dir.display());
+            println!("cargo:rustc-link-lib=static=dilithium3_clean");
+        },
+        _ => {
+            panic!("unsupported target {}", target);
+        }
+    }
 }
