61932888258b7c5e88ce6af3fa112329fcbe4f2f
gcsbt <150135295+gcsbt@users.noreply.github.com>
2024-02-15 10:20:59 +0100

    kyber768key.rs: use PQCLEAN_KYBER768_CLEAN_CRYPTO_SECRETKEYBYTES

--- src/kyber768key.rs
+++ src/kyber768key.rs
@@ -1,14 +1,13 @@
-pub(crate) use ctap_types::{
-    Bytes
-};
 use crate::{Error, Result};
+pub(crate) use ctap_types::Bytes;
+use pqcrypto_kyber::ffi::PQCLEAN_KYBER768_CLEAN_CRYPTO_SECRETKEYBYTES;
 pub(crate) type SerializedKeyBytes = trussed::types::MessagePQ;
 
 #[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct Key {
     pub kind: Bytes<16>,
-    pub material: Bytes<2400>,
- }
+    pub material: Bytes<PQCLEAN_KYBER768_CLEAN_CRYPTO_SECRETKEYBYTES>,
+}
 
 impl Key {
     pub fn serialize(&self) -> Result<SerializedKeyBytes> {
@@ -19,7 +18,7 @@ impl Key {
         trussed::cbor_deserialize(&serialized_data).map_err(|_| Error::Other)
     }
 
-    pub fn get_material(&self) -> &Bytes<2400> {
+    pub fn get_material(&self) -> &Bytes<PQCLEAN_KYBER768_CLEAN_CRYPTO_SECRETKEYBYTES> {
         &self.material
     }
 }
