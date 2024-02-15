ff1767b4733fc7c976689c643ea57f8d7f54603a
gcsbt <150135295+gcsbt@users.noreply.github.com>
2024-02-15 13:59:46 +0100

    failure-getrandom: don't return an error

--- failure-getrandom/src/lib.rs
+++ failure-getrandom/src/lib.rs
@@ -2,12 +2,9 @@
 
 extern crate getrandom;
 
-use core::num::NonZeroU32;
 use getrandom::Error;
 
-// Some application-specific error code
-const MY_CUSTOM_ERROR_CODE: u32 = Error::CUSTOM_START + 42;
 pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
-    let code = NonZeroU32::new(MY_CUSTOM_ERROR_CODE).unwrap();
-    Err(Error::from(code))
+    // TODO using trussed.random_bytes
+    Ok(())
 }
